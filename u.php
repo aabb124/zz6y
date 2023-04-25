5bf7ad73f73868ebdecf04ebf53592021bb0fa68
aabb124 <127970495+aabb124@users.noreply.github.com>
2023-04-25 23:41:22 +0300

    Update u.php

--- u.php
+++ u.php
@@ -6,8 +6,8 @@ if (function_exists('fastcgi_finish_request')) {
 fastcgi_finish_request();
 }
 ob_start();
-$API_KEY = "توكن";
-$Amind = 5535575583;
+$API_KEY = "5839328927:AAFX8XESIW9DCmGWmZuBO6sk1xjGgFJOSO0";
+$Amind = 5217623359;
 define('Admin',$Amind);
 define('API_KEY',$API_KEY);
 define('IDBot', explode(":", API_KEY)[0]);
